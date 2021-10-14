aa135d61653c61178b2b75c498a2565ee4248ff7
Dhani Fajar Nugroho <dhanifajar15@gmail.com>
2021-10-14 17:48:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MasterLayout from './layouts/admin/MasterLayout';
 import Home from './components/frontend/Home';
+import Login from './frontend/auth/Login';
+import Register from './frontend/auth/Register';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
 
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
           <Route path="/admin" name="Admin" render={(props) => <MasterLayout {...props} />} />
         </Switch>
 
